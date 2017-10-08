cce0b507a25d02fa00cad2219aaaeb173028f730
Brent  Ransom <sadumedusa@gmail.com>
2017-10-08 22:17:08 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,12 @@
 def display_board(board)
-  puts"   |   |   "
-  puts"-----------"
-  puts"   |   |   "
-  puts"-----------"
-  puts"   |   |   "
+  rows[0]="   |   |   "
+  rows[1]="-----------"
+  rows[2] ="   |   |   "
+  rows[3]="-----------"
+  rows[4]"   |   |   "
 
   board = [" "," "," "," "," "," "," "," "," ",]
+  
 end
 
   
