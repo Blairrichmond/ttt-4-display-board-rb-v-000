99876ec7c809b7084353fc5f1522b9173f56d05c
Brent  Ransom <sadumedusa@gmail.com>
2017-10-08 20:52:10 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,11 @@
 def display_board(board)
+  board = [" "," "," "," ","X"," "," "," "," "]
+    
   puts"   |   |   "
   puts"-----------"
   puts"   |   |   "
   puts"-----------"
   puts"   |   |   "
 
-  board = [" "," "," "," ","X"," "," "," "," "]
+
 end
